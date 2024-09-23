0ec3c52d0a3c4cdb1a3bfa7f987661a110432238
merlin <103051006+merl1nnn@users.noreply.github.com>
2024-09-23 20:20:39 +0200

    Internalization

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('i18n.urls', namespace='i18n'))
+]
