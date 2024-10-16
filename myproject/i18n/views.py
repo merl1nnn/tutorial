c0695b27938a6d271717e1bfc4e92450d73d4ce1
Merlin <103051006+merl1nnn@users.noreply.github.com>
2024-10-16 14:50:26 +0200

    Update views.py

--- myproject/i18n/views.py
+++ myproject/i18n/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-
 # Create your views here.
 def home (request):
-    trans = _('home')
-    return render(request,'home.html', {'trans': trans})
+    welcomeMessage = _('Welcome to this Django Internationalization Guide!')
+    return render(request,'home.html', {'trans': welcomeMessage})
