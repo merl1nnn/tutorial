9e0693ea9505cf8fcfef419aeebef4c93590c5d6
Merlin <103051006+merl1nnn@users.noreply.github.com>
2024-10-16 14:49:43 +0200

    Update urls.py

--- myproject/i18n/urls.py
+++ myproject/i18n/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-
 app_name = 'i18n'
-
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='Welcome to this Django Internationalization Guide!'),
 ]
